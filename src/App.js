7633c9f300dba4d34185712e15a962790d450938
Ahmad Alfaify <a7-h-m@outlook.com>
2021-06-24 16:29:58 +0300

    change some text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          simple CI CD projekt 
+          simple CI CD project 
         </a>
       </header>
     </div>
